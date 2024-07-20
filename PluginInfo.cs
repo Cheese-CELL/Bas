a4e8642e051ea316a7e5b02fd60bc99e5a95867a
Cheese <mosstionz@gmail.com>
2024-07-20 09:05:04 -0400

    Added Source

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Bas
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.Cheese.gorillatag.BAS";
+		public const string Name = "BAS";
+		public const string Version = "1.0.0";
+	}
+}
